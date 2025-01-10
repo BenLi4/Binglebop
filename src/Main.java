7e2631a8fc011108c280f932aed7b55866bb75e6
BT_1N3_18 <benjaminl157@nycstudents.net>
2025-01-10 13:58:25 -0500

    Things

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Blah");
-        System.out.println("Pain");
+        System.out.println("Spain");
         System.out.println("GEHAHAHAHAHAHAHA");
         System.out.println("YOLO");
     }
