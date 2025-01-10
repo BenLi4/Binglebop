89472eb0a30cd4094eb5edd3637c7f9231c49f37
BT_1N3_18 <benjaminl157@nycstudents.net>
2025-01-10 13:52:42 -0500

    Things

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Blah");
         System.out.println("Pain");
+        System.out.println("GEHAHAHAHAHAHAHA");
     }
 }
---
+++ src/Si.java
@@ -0,0 +1,6 @@
+public class Si {
+    private int a;
+    public Si(int v) {
+        a = v;
+    }
+}
