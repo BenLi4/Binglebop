85f02d9d2825b771231ab05f50cba1fde6c0413d
BT_1N3_17 <Andyy41@nycstudents.net>
2025-01-10 13:58:28 -0500

    Life is Pain, YOLO , pizza

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         System.out.println("Blah");
         System.out.println("Pain");
         System.out.println("GEHAHAHAHAHAHAHA");
-        System.out.println("YOLO");
+        System.out.println("Pizza Time");
     }
 }
